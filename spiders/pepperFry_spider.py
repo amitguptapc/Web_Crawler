be09192dc6587b4b9723d3da4a08c3ff4f997c6f
Amit <amitguptapc@gmail.com>
2020-02-24 01:13:37 +0530

    Fixed bugs in image scraping

--- spiders/pepperFry_spider.py
+++ spiders/pepperFry_spider.py
@@ -6,7 +6,7 @@ import requests
 
 class PepperFrySpider(scrapy.Spider):
     name = 'PepperFrySpider'
-    base_dir = './PepperFry_data/'
+    base_dir = 'PepperFry/PepperFry_data/'
     max_count = 20
 
     def start_requests(self):
@@ -33,11 +33,10 @@ class PepperFrySpider(scrapy.Spider):
             yield resp
 
     def parse(self,response,**meta):
-        product_urls = response.css('//div/div/div/a[@p=0]/@href').extract()
-        print(product_urls)
-
+        product_urls = response.css('div#productView div.clip-dtl-ttl a::attr(href)').getall()
         counter = 0
         for url in product_urls:
+            url = url[:url.find('?')]
             resp = scrapy.Request(url=url,callback=self.parse_item,dont_filter=True)
             resp.meta['dir_name'] = response.meta['dir_name']
             if counter == self.max_count:
@@ -47,29 +46,24 @@ class PepperFrySpider(scrapy.Spider):
             yield resp
 
     def parse_item(self,response,**meta):
-        item_title = respose.xpath('//div/div/div/h1/text()').extract()[0]
-        item_price = response.xpath('//div/div/div/p/b[@class="pf-orange-color pf-large font-20 pf-primary-color"]/text()').extract()[0].strip()
-        item_savings = response.xpath('//p[@class=pf-margin-0 pf-bold-txt font-13]/text()').extract()[0].strip()
-        item_description = response.xpath('//div[@item_prop="description"]/div/p/text()').extract()
+        item_title = response.css('div div div h1::text').get()
+        item_price = response.css('div div p b.pf-orange-color::attr(data-price)').get()
+        item_savings = response.css('div.pf-padding-7 div.sm-9 p.pf-margin-0::text').get()
         item_detail_keys = response.xpath('//div[@id="itemDetail"]/p/b/text()').extract()
         item_detail_values = response.xpath('//div[@id="itemDetail"]/p/text()').extract()
         brand = itemprop = response.xpath('//span[@itemprop="brand"]/text()').extract()
         item_detail_values[0] = brand[0]
-        stopwords = ["(all dimensions in inches)","(all dimensions in inches)","(all dimensions in inches)"]
-        item_detail_values = [word.strip() for word in item_detail_values if word not in stopwords]
         a = len(item_detail_keys)
         b = len(item_detail_values)
+        stopwords = ["(all dimensions in inches)","(all dimensions in inches)","(all dimensions in inches)"]
+        item_detail_values = [word.strip() for word in item_detail_values if word not in stopwords]
         idetail = {}
         for i in range(min(a,b)):
             idetail[item_detail_keys[i]] = item_detail_values[i]
-        stop_items = ["pepperfry.com","We also offer you a","So go ahead and buy with confidence","Brand will upfront contact you for assembly"]
-        item_description = filter(lambda x: all([not y.lower() in x.lower() for y in stop_items]),item_description)
-        item_description = '\n'.join(item_description)
         image_url_list = response.xpath('//li[@class="vip-options-slideeach"]/a/@data-img').extract()
         if len(image_url_list)>3:
             d = {
             'Item Title' : item_title,
-            'Description' : item_description,
             'Item Price' : item_price,
             'Savings' : item_savings,
             'Details' : idetail
@@ -83,7 +77,7 @@ class PepperFrySpider(scrapy.Spider):
             for i,image_url in enumerate(image_url_list):
                 r = requests.get(image_url)
                 with open(os.path.join(item_dir_url,'image_{}.jpg'.format(i)),'wb') as f:
-                    f.wrie(r.content)
+                    f.write(r.content)
             print("Saved Successfully "+item_title+" data at "+item_dir_url)
             yield d
         yield None
